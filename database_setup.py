468919f51b3778a6c74e6ff29f99ab7e83fc555c
Rajshree  Phadol <rajshree.phadol28@gmail.com>
2018-06-29 06:30:50 -0400

    Add files via upload

--- database_setup.py
+++ database_setup.py
@@ -10,16 +10,17 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
-    email = Column(String(250), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
     picture = Column(String(250))
+
 
 class Category(Base):
     __tablename__ = 'category'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(255), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User, backref="category")
 
@@ -27,9 +28,10 @@ class Category(Base):
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
-            'name'          : self.name,
-            'id'            : self.id
+            'name': self.name,
+            'id': self.id
         }
+
 
 class Items(Base):
     __tablename__ = 'items'
@@ -39,7 +41,8 @@ class Items(Base):
     date = Column(DateTime, nullable=False)
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category, backref=backref('items', cascade='all, delete'))
+    category = relationship(
+        Category, backref=backref('items', cascade='all, delete'))
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User, backref="items")
 
@@ -47,10 +50,10 @@ class Items(Base):
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
-            'name'          : self.name,
-            'id'            : self.id,
-            'description'   : self.description,
-            'category'      : self.category.name
+            'name': self.name,
+            'id': self.id,
+            'description': self.description,
+            'category': self.category.name
         }
 
 
--- lotsofdata.py
+++ lotsofdata.py
@@ -22,61 +22,60 @@ session = DBSession()
 
 # Create dummy user
 User1 = User(name="Vira ", email="angel@udacity.com",
-             picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+             picture='https://pbs.twimg.com/profile_images/2671170543/18de  \
+                      bd694829ed78203a5a36dd364160_400x400.png')
 session.add(User1)
 session.commit()
 
 
 Category1 = Category(name="Hockey",
-                      user_id=1)
+                     user_id=1)
 session.add(Category1)
 session.commit()
 
 Category2 = Category(name="Snowboarding",
-                      user_id=1)
+                     user_id=1)
 session.add(Category2)
 session.commit
 
 Category3 = Category(name="Rock Climbing",
-                      user_id=1)
+                     user_id=1)
 session.add(Category3)
 session.commit()
 
 Category4 = Category(name="Skating",
-                      user_id=1)
+                     user_id=1)
 session.add(Category4)
 session.commit()
 
 Category5 = Category(name="Football",
-                      user_id=1)
+                     user_id=1)
 session.add(Category5)
 session.commit()
 
 
 Item1 = Items(name="Stick",
-               date=datetime.datetime.now(),
-               description="Built for the advanced player with 70% carbon construction.",
-               category_id=1,
-               user_id=1)
+              date=datetime.datetime.now(),
+              description="Built with 70% carbon construction.",
+              category_id=1,
+              user_id=1)
 session.add(Item1)
 session.commit()
 
 Item2 = Items(name="Hockey Field Ball",
-               date=datetime.datetime.now(),
-               description="Official size and weight. Solid white color ball.",
-               category_id=1,
-               user_id=1)
+              date=datetime.datetime.now(),
+              description="Official size and weight. Solid white color ball.",
+              category_id=1,
+              user_id=1)
 session.add(Item2)
 session.commit()
 
 Item3 = Items(name="Hockey Socks",
-               date=datetime.datetime.now(),
-               description="socks are made of Cotton/Polyester blend,elasticized ankle to fit any ankle machine washable ",
-               category_id=1,
-               user_id=1)
+              date=datetime.datetime.now(),
+              description="socks are made of Cotton/Polyester blend,",
+              category_id=1,
+              user_id=1)
 session.add(Item3)
 session.commit()
-
-
 
 print "added menu items!"
